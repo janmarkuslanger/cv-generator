ea9fc1ee208e63ff24d6af226aa75eaafde2f613
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2024-11-10 13:17:48 +0100

    add classes to data module

---
+++ cv_generator/data/__init__.py
@@ -0,0 +1,16 @@
+from .data_source import DataSource
+from .personal_info import PersonalInfo
+from .address import Address
+from .work_experience import WorkExperience
+from .skill import Skill
+from .education import Education
+
+
+__all__ = [
+    "DataSource",
+    "PersonalInfo",
+    "Address",
+    "WorkExperience",
+    "Skill",
+    "Education"
+]
